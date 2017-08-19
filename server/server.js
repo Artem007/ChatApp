f1c020e63286817dc06bfd13e2bc05b76dc52f41
Artem <melnykartem10@gmail.com>
2017-08-19 19:40:23 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path=require('path');
+const http=require('http');
+
+const express=require('express');
+
+var app=express();
+var server=http.createServer(app);
+var publicPath=path.join(__dirname+'/../public');
+
+app.use(express.static(publicPath));
+
+server.listen(3000,()=>{
+  console.log('Server has started on port 3000');
+});
