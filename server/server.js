9d0c69046d81933c3b6ab218310c55e5dea4114f
Artem <melnykartem10@gmail.com>
2017-08-19 20:51:12 +0200

    Add connection and disconnect socket

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
-const path=require('path');
-const http=require('http');
+const path = require('path');
+const http = require('http');
 
-const express=require('express');
+const express = require('express');
+const socketIO = require('socket.io');
 
-var app=express();
-var server=http.createServer(app);
-var publicPath=path.join(__dirname+'/../public');
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+var publicPath = path.join(__dirname + '/../public');
 
 app.use(express.static(publicPath));
 var port = process.env.PORT || 3000;
 
-server.listen(port,()=>{
+io.on('connection', (socket) => {
+  console.log('New clinet');
+
+  socket.on('disconnect', () => {
+    console.log('disconnected from client');
+  });
+
+})
+
+server.listen(port, () => {
   console.log('Server has started on port 3000');
 });
